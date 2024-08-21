3853b82249acd22602ba1bd211b72c184035f23d
HARINI <106902045+HARINIMANIKANDAN1234@users.noreply.github.com>
2024-08-22 04:44:22 +0530

    Add files via upload

---
+++ Ans2/index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let goalPercentage = 40; // This would be dynamic based on data
+    document.getElementById('goal-percentage').textContent = goalPercentage + '%';
+});
